2ec2fa04d2497fcb9d495ebc4bd53107eed5ce50
Phương JSP <phuong.jsp@gmail.com>
2018-04-03 23:30:17 +0700

    test client by http://websystique.com/springmvc/spring-mvc-4-angularjs-example/

---
+++ src/main/java/hoang/phuong/server/controller/ControllerIndex.java
@@ -0,0 +1,13 @@
+package hoang.phuong.server.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class ControllerIndex {
+    @RequestMapping(method = RequestMethod.GET)
+    public String getIndexPage() {
+        return "UserManagement";
+    }
+}
